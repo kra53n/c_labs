ebc7d16728eed62616e7071a4df9a59f47f651bd
Krai53n <greasha46@gmail.com>
2022-02-26 18:01:23 +0300

    rewrite

--- 2_term/practice/part3/p03_01_01_02_Бахтин.cpp
+++ 2_term/practice/part3/p03_01_01_02_Бахтин.cpp
@@ -1,41 +1,76 @@
 #include <stdio.h>
+#include <malloc.h>
+
+int countSymbol(char* string, char symbol)
+{
+    int num = 0;
+    for (int i = 0; string[i] != 0; i++)
+        if (string[i] == symbol)
+            num += 1;
+    return num;
+}
+
+int strLen(char* string)
+{
+    int num = 0;
+    for (int i = 0; string[i] != 0; i++)
+        num += 1;
+    return num;
+}
+
+char* removeSymbolInString(char* string, char symbol)
+{
+    char* resString = (char*)malloc(sizeof(char));
+    int j;
+    for (int i = j = 0; string[i] != 0; i++)
+        if (string[i] != symbol)
+        {
+            resString = (char*)realloc(resString, (j + 1) * sizeof(char));
+            resString[j++] = string[i];
+        }
+    resString[j] = '\0';
+    return resString;
+}
+
+char* genereteStrOfSymbols(int num, char symbol)
+{
+    char* str = (char*)malloc(sizeof(char));
+    int i = 0;
+    while (i < num)
+    {
+        str = (char*)realloc(str, (i+1) * sizeof(char));
+        str[i++] = symbol;
+    }
+    str[i] = '\0';
+    return str;
+}
+
+void strCpy(char* str1, char* str2)
+{
+    int str1Len = strLen(str1);
+    int i = 0;
+    while (str2[i] != 0)
+    {
+        str1 = (char*)realloc(str1, (str1Len + i + 1) * sizeof(char));
+        str1[str1Len + i++] = str2[i];
+    }
+    str1[str1Len + i] = '\0';
+}
 
 int main()
 {
-	char testString[] = "  хаха  ";
-	char resString[100];
-	char resStringSpaces[100];
+	char testString[] = "  хаха  so niiiiiceeeee      eeeeeee";
+    char* newString = removeSymbolInString(testString, ' ');
+    int numSpaces = countSymbol(testString, ' ');
 
-	printf("%s\n", testString);
+    printf("\n%s. Num of spaces: %d\n", newString, numSpaces);
 
-	int i, j = 0;
-	int numSpaces = 0;
-	for (i = 0; testString[i] != '\0'; i++)
-	{
-		if (testString[i] != ' ')
-		{
-			resString[j++] = testString[i];
-			printf("\n%d %c", j, resString[j]);
-		}
-		else
-			numSpaces++;
-	}
-	resString[j] = '\0';
+    strCpy(newString, "I'm string");
+    strCpy(newString, "I'm string");
+    strCpy(newString, genereteStrOfSymbols(numSpaces, '1'));
 
-	for (int i = j; i > j / 2; i--)
-	{
-		resString[i + numSpaces] = resString[i];
-		printf("\n%s\n", resString);
-	}
-	for (int i = j / 2; i < j / 2 + numSpaces; i++)
-	{
-		resString[i] = ' ';
-	}
-
-
-	printf("%s\n", resString);
-
-	printf("\nКоличество пробелов: %d", numSpaces);
+    printf("\n%s. Num of spaces: %d\n", genereteStrOfSymbols(numSpaces, ' '), numSpaces);
+    printf("\n%s. Num of spaces: %d\n", newString, numSpaces);
 
 	return 0;
 }
