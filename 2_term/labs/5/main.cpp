08ad3cd30b0df4be05b07f3e341595e0db7fee3e
Krai53n <greasha46@gmail.com>
2022-03-05 12:34:21 +0300

    generating circle coords

--- 2_term/labs/5/main.cpp
+++ 2_term/labs/5/main.cpp
@@ -1,10 +1,21 @@
 #include <SDL2/SDL.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
 SDL_Window*   win = NULL;
 SDL_Renderer* ren = NULL;
 
 int winWdt = 800;
 int winHgt = 600;
+
+struct Circ
+{
+    float x;
+    float y;
+    float r;
+    bool isDrawing;
+};
 
 void init()
 {
@@ -15,6 +26,7 @@ void init()
         winWdt, winHgt, SDL_WINDOW_SHOWN
     );
     ren = SDL_CreateRenderer(win, -1, 0);
+    srand(time(0));
 }
 
 void deInit()
@@ -24,7 +36,7 @@ void deInit()
     SDL_Quit();
 }
 
-void drawCircle(int x, int y, int r)
+void drawCircle(int x, int y, float r)
 {
     float rad = M_PI / 180;
     for (int i = 0, step = 5; i < 360; i += step)
@@ -39,6 +51,29 @@ void drawCircle(int x, int y, int r)
     }
 }
 
+float randFloat(float min, float max)
+{
+    return ((float)rand() / RAND_MAX) * (max - min) + min;
+}
+
+void generateCircCoords(Circ circs[], int num, float min_r=2.5, float max_r=25)
+{
+    int border = 10;
+    for (int i = 0; i != 10; i++)
+    {
+        circs[i].r = randFloat(min_r, max_r);
+        circs[i].x = randFloat(circs[i].r + border, winWdt - circs[i].r - border);
+        circs[i].y = randFloat(circs[i].r + border, winHgt - circs[i].r - border);
+        circs[i].isDrawing = false;
+
+        // printf("\nCircle %d\n", i);
+        // printf("\tr: %f", circs[i].r);
+        // printf("\tx: %f", circs[i].x);
+        // printf("\ty: %f", circs[i].y);
+        // printf("\tisDrawing: %d", circs[i].isDrawing);
+    }
+}
+
 void mainLoop()
 {
     SDL_Event event;
@@ -47,14 +82,15 @@ void mainLoop()
     while (isRunning)
     {
         SDL_PollEvent(&event);
-        }
     }
 }
 
 int main()
 {
     init();
-    mainLoop();
+    //mainLoop();
+    int num = 10; Circ circs[num];
+    createCircCoords(circs, num);
     deInit();
 
     return 0;
