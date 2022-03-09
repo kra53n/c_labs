05657e48af282dda7e5d9a82671f8bf503a0d403
Krai53n <greasha46@gmail.com>
2022-03-09 18:49:37 +0300

    refactoring code

--- 2_term/labs/2/main.cpp
+++ 2_term/labs/2/main.cpp
@@ -1,52 +1,68 @@
 #include <SDL2/SDL.h>
 
-const int SCREEN_WIDTH  = 700;
-const int SCREEN_HEIGHT = 800;
+SDL_Window*  win        = NULL;
+SDL_Surface* screenSurf = NULL;
+
+int winWdt = 700;
+int winHgt = 800;
+
+
+void deInit(char error)
+{
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+    exit(error);
+}
+
+void init()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("SDL couldn't init! SDL_Error: %s\n", SDL_GetError());
+        deInit(1);
+    }
+
+    win = SDL_CreateWindow("Bakhtin SDL lab", SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED, winWdt, winHgt, SDL_WINDOW_SHOWN
+    );
+    if (win == NULL)
+    {
+        printf("SDL couldn't init! SDL_Error: %s\n", SDL_GetError());
+        deInit(1);
+    }
+
+}
+
+SDL_Surface* loadMedia(char filename[])
+{
+    SDL_Surface* media = SDL_LoadBMP(filename);
+    if (media == NULL)
+    {
+        printf("Unable to load media %s! SDL_Error:%s\n", filename, SDL_GetError());
+        deInit(1);
+    }
+    return media;
+}
 
 int main()
 {
-    SDL_Window* window = NULL;
-    SDL_Surface* screenSurface = NULL;
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-    }
-    else
-    {
-        window = SDL_CreateWindow("Бахтин", SDL_WINDOWPOS_UNDEFINED,
-            SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (window == NULL)
-        {
-            printf("Window could not initialize! SDL_Error: %s\n", SDL_GetError());
-        }
-        else
-        {
-            screenSurface = SDL_GetWindowSurface(window);
-            SDL_FillRect(screenSurface, NULL,
-                SDL_MapRGB(screenSurface->format, 31, 73, 125));
+    init();
 
-            char imgPath[] = "img.bmp";
-            SDL_Surface* img = SDL_LoadBMP(imgPath);
-            if (img == NULL)
-            {
-                printf("Unable to load image %s! SDL_Error: %s\n", imgPath, SDL_GetError());
-            }
-            else
-            {
-                SDL_Rect imgRect;
-                imgRect.x = SCREEN_WIDTH - img->w;
-                imgRect.y = SCREEN_HEIGHT - img->h;
-                SDL_BlitSurface(img, NULL, screenSurface, &imgRect);
-            }
+    screenSurf = SDL_GetWindowSurface(win);
+    SDL_FillRect(screenSurf, NULL, SDL_MapRGB(screenSurf->format, 31, 73, 125));
 
-            SDL_UpdateWindowSurface(window);
-            SDL_Delay(2000);
-        }
-    }
+    SDL_Surface* img = loadMedia("img.bmp");
 
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    SDL_Rect imgRect;
+    imgRect.x = winWdt - img->w;
+    imgRect.y = winHgt - img->h;
+    SDL_BlitSurface(img, NULL, screenSurf, &imgRect);
+
+    SDL_UpdateWindowSurface(win);
+    SDL_Delay(2000);
+
+    deInit(0);
 
     return 0;
 }
