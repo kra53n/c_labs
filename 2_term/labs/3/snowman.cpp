6d238cfbde20a1ac88fffb1a7c6b212a48149656
Krai53n <greasha46@gmail.com>
2022-03-13 12:56:03 +0300

    add nose

--- 2_term/labs/3/snowman.cpp
+++ 2_term/labs/3/snowman.cpp
@@ -1,14 +1,16 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
+#include <math.h>
 
 SDL_Window*   win = NULL;
 SDL_Renderer* ren = NULL;
 
 int winWdt = 800;
 int winHgt = 600;
-
 int winWdt2 = winWdt / 2;
 int winHgt2 = winHgt / 2;
+
+const float RAD = M_PI / 180;
 
 void deInit(char error)
 {
@@ -85,26 +87,76 @@ void drawSnowman(int x, int y)
 
     // body
     SDL_RenderDrawRect(ren, &snowRect);
-    drawFillCirc((snowRect.x + snowRect.w / 2), (snowRect.y + snowRect.h) - snowRect.w * 0.45, snowRect.w * 0.45);
-    drawFillCirc((snowRect.x + snowRect.w / 2), (snowRect.y + snowRect.h) - snowRect.w * 1.1, snowRect.w * 0.38);
-    drawFillCirc((snowRect.x + snowRect.w / 2), (snowRect.y + snowRect.h) - snowRect.w * 1.6, snowRect.w * 0.3);
+    drawFillCirc(
+        snowRect.x + snowRect.w / 2,
+        snowRect.y + snowRect.h - snowRect.w * 0.45,
+        snowRect.w * 0.45
+    );
+    drawFillCirc(
+        snowRect.x + snowRect.w / 2,
+        snowRect.y + snowRect.h - snowRect.w * 1.1,
+        snowRect.w * 0.38
+    );
+    drawFillCirc(
+        snowRect.x + snowRect.w / 2,
+        snowRect.y + snowRect.h - snowRect.w * 1.6,
+        snowRect.w * 0.3
+    );
     
     // legs
     SDL_SetRenderDrawColor(ren, 255, 0, 0, 0);
-    drawFillHalfUpCirc((snowRect.x + snowRect.w * 0.6 / 2), (snowRect.y + snowRect.h), snowRect.w * 0.15);
-    drawFillHalfUpCirc((snowRect.x + snowRect.w * 1.4 / 2), (snowRect.y + snowRect.h), snowRect.w * 0.15);
+    drawFillHalfUpCirc(
+        snowRect.x + snowRect.w * 0.6 / 2,
+        snowRect.y + snowRect.h,
+        snowRect.w * 0.15
+    );
+    drawFillHalfUpCirc(
+        snowRect.x + snowRect.w * 1.4 / 2,
+        snowRect.y + snowRect.h,
+        snowRect.w * 0.15
+    );
 
     // eyes
     SDL_SetRenderDrawColor(ren, 255, 0, 0, 0);
-    drawFillCirc((snowRect.x + snowRect.w * 0.77 / 2), (snowRect.y + snowRect.h) - snowRect.w * 1.7, snowRect.w * 0.04);
+    drawFillCirc(
+        snowRect.x + snowRect.w * 0.77 / 2,
+        snowRect.y + snowRect.h - snowRect.w * 1.7,
+        snowRect.w * 0.04
+    );
     SDL_SetRenderDrawColor(ren, 0, 0, 255, 0);
-    drawFillCirc((snowRect.x + snowRect.w * 1.23 / 2), (snowRect.y + snowRect.h) - snowRect.w * 1.7, snowRect.w * 0.04);
+    drawFillCirc(
+        snowRect.x + snowRect.w * 1.23 / 2,
+        snowRect.y + snowRect.h - snowRect.w * 1.7,
+        snowRect.w * 0.04
+    );
 
     // buttons
     SDL_SetRenderDrawColor(ren, 0, 0, 0, 0);
     for (float i = 0.2; i < 1.3; i += 0.2)
+        drawFillCirc(
+            snowRect.x + snowRect.w / 2,
+            snowRect.y + snowRect.h - snowRect.w * i,
+            snowRect.w * 0.02
+        );
+
+    // smile
+    for (int i = -180, step = 10; i < 0; i += step)
+        drawFillCirc(
+            snowRect.x + snowRect.w / 2 - (cos(i * RAD) * snowRect.w * 0.05),
+            snowRect.y + snowRect.h - snowRect.w * 1.55 - (sin(i * RAD) * snowRect.w * 0.03),
+            snowRect.w * 0.01
+        );
+
+    // nose
+    SDL_SetRenderDrawColor(ren, 225, 138, 31, 0);
+    //printf("snowRect.w * 0.03: %f", snowRect.w * 0.03);
+    for (float i = 0; i < snowRect.w * 0.03; i++)
     {
-        drawFillCirc((snowRect.x + snowRect.w / 2), (snowRect.y + snowRect.h) - snowRect.w * i, snowRect.w * 0.02);
+        drawFillCirc(
+            snowRect.x + snowRect.w / 2 + snowRect.w * i * 0.01,
+            snowRect.y + snowRect.h - snowRect.w * 1.6 + snowRect.w * i * 0.003,
+            snowRect.w / (i*3 + 50)
+        );
     }
  }
 
@@ -113,7 +165,7 @@ int main(int argc, char* argv[])
     init();
     drawSnowman(winWdt2, winHgt2);
     SDL_RenderPresent(ren);
-    SDL_Delay(15000);
+    SDL_Delay(3000);
     deInit(0);
 
     return 0;
