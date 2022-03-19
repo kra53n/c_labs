ab0f4dbb2bcbc120c1fe4c9ccdaf94a77368b66a
Krai53n <greasha46@gmail.com>
2022-03-19 22:18:50 +0300

    add completed snawfalling effect

--- 2_term/labs/4/snowfall.cpp
+++ 2_term/labs/4/snowfall.cpp
@@ -5,7 +5,7 @@
 #include <math.h>
 #include <time.h>
 
-SDL_Window*   win = NULL;
+SDL_Window* win = NULL;
 SDL_Renderer* ren = NULL;
 
 int winWdt = 800;
@@ -17,7 +17,7 @@ struct Snowflake
 {
     int x;
     int y;
-    int direction;
+    float direction;
     int sign;
     float scale;
     float r;
@@ -26,8 +26,8 @@ struct Snowflake
 struct Snowfall
 {
     Snowflake** array = NULL;
-    int row;
-    int col;
+    int row = 0;
+    int col = 100;
     SDL_Color color;
 };
 
@@ -49,7 +49,7 @@ void init()
         deInit(1);
     }
 
-    win = SDL_CreateWindow(u8"Бахтин", SDL_WINDOWPOS_CENTERED,
+    win = SDL_CreateWindow(u8"Снегопад", SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED, winWdt, winHgt, SDL_WINDOW_SHOWN
     );
     if (win == NULL)
@@ -87,20 +87,20 @@ float randFloat(float min, float max)
     return ((float)rand() / RAND_MAX) * (max - min) + min;
 }
 
-void generateSnowflake(Snowflake &s, int x, int y)
+void generateSnowflake(Snowflake& s, int x, int y)
 {
 
     s.direction = rand() % 90 + 225;
     s.sign = rand() % 2 ? 1 : -1;
 
     s.r = randFloat(2, 5);
-    s.scale = 200;
+    s.scale = 100 / s.r;
 
-    s.x = (rand() % 10 + 2) * (int)(30 / s.r);
+    s.x = x + (rand() % 10 + 2) * (int)(30 / s.r);
     s.y = y;
 }
 
-void addSnowflakesLayer(Snowfall &s)
+void addSnowflakesLayer(Snowfall& s)
 {
     s.array = (Snowflake**)realloc(s.array, sizeof(Snowflake) * ++s.row);
     if (s.array == NULL)
@@ -108,19 +108,19 @@ void addSnowflakesLayer(Snowfall &s)
         printf("\nMemory cannot be allocated!\n");
         exit(1);
     }
-    
-    s.array[s.row] = (Snowflake*)malloc(sizeof(Snowflake) * s.col);
-    if (s.array[s.row] == NULL)
+
+    s.array[s.row-1] = (Snowflake*)malloc(sizeof(Snowflake) * s.col);
+    if (s.array[s.row-1] == NULL)
     {
         printf("\nMemory cannot be allocated!\n");
         exit(1);
     }
 
     for (int i = 0; i < s.col; i++)
-        generateSnowflake(s.array[s.row][i], 10 * (rand() % 10), 10 * (rand() % 10));
+        generateSnowflake(s.array[s.row-1][i], -500 + (rand() % winWdt), -1000 + (100 * (rand() % 10)));
 }
 
-void drawSnow(Snowflake &s)
+void drawSnow(Snowflake& s)
 {
     drawFillCirc(
         cos(s.direction * RAD) * s.scale + s.x,
@@ -129,48 +129,65 @@ void drawSnow(Snowflake &s)
     );
 }
 
-void changeSnowflakePosition(Snowflake &s)
+void changeSnowflakePosition(Snowflake& s)
 {
-    s.direction += s.sign * (s.scale / 10 + (10 / s.r)) * 0.01;
+    s.direction += s.sign * (s.scale / 10 + (2 / s.r)) * 0.01;
 
-    if (s.direction <= 180)
+    if (s.direction < 180)
     {
         s.sign *= -1;
         s.direction = 180;
     }
-    if (s.direction >= 360)
+    if (s.direction > 360)
     {
         s.sign *= -1;
         s.direction = 360;
     }
 
     s.y += s.r * 0.7;
-    s.x += s.sign * (1 / s.r);
+    s.x += s.r * 0.44;
+}
+
+void mainLoop()
+{
+    SDL_Event ev;
+
+    Snowfall snowfall;
+    addSnowflakesLayer(snowfall);
+
+    bool isRunning = true;
+    while (isRunning)
+    {
+        SDL_PollEvent(&ev);
+        if (ev.type == SDL_QUIT)
+            isRunning = false;
+
+        SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
+        SDL_RenderClear(ren);
+        SDL_SetRenderDrawColor(ren, 255, 255, 255, 0);
+
+        for (int p = 0; p < snowfall.row; p++)
+        {
+            for (int j = 0; j < snowfall.col; j++)
+            {
+                drawSnow(snowfall.array[p][j]);
+                changeSnowflakePosition(snowfall.array[p][j]);
+            }
+        }
+
+        if (SDL_GetTicks() % 150 == 0)
+            addSnowflakesLayer(snowfall);
+
+        SDL_Delay(10);
+        SDL_RenderPresent(ren);
+    }
 }
 
 int main(int argc, char* argv[])
 {
     init();
-    
-    Snowfall snowfall;
-    addSnowflakesLayer(snowfall);
-
-    for (int i = 0; i < 800; i++)
-    {
-        SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
-        SDL_RenderClear(ren);
-        SDL_SetRenderDrawColor(ren, 255, 255, 255, 0);
-
-        for (int j = 0; j < 100; j++)
-        {
-            drawSnow(snowfall.array[0][j]);
-            changeSnowflakePosition(snowfall.array[0][j]);
-        }
-
-        SDL_Delay(10);
-        SDL_RenderPresent(ren);
-    }
-
+    mainLoop();
     deInit(0);
+
     return 0;
 }
