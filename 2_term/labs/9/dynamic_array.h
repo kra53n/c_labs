6316391d2326b4f2d86e1546d4d6b070e4e0a4b0
Krai53n <greasha46@gmail.com>
2022-03-28 18:03:32 +0300

    fix

--- 2_term/labs/9/dynamic_array.h
+++ 2_term/labs/9/dynamic_array.h
@@ -1,17 +1,25 @@
 #ifndef DYNARRAY_H
 #define DYNARRAY_H
 
+int askUserAboutNumber(char const title[]);
+
 float** mallocArray2D();
 
 float** reallocArray2D(float** arr, int num);
 
-void mallocArray2DForArray(float **arr, int row, int cols);
+int getArray2DSize(float** arr);
+
+float** getArray2DWithAddedRow(float** arr, int row, int cols);
+
+float** getArray2DWithDeletedRow(float** arr, int row);
+
+void mallocArray2DForArray(float** arr, int row, int cols);
 
 void freeArray(float** arr);
 
-void fillArrayManually(float **arr);
+float** getFilledArrayManually(float** arr);
 
-void printArray2D(float **arr);
+void printArray2D(float** arr);
 
 float** getArrayFromFile(float** arr, char filename[]);
 
