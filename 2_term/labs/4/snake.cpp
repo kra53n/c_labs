e9ae46348d0a561fc9dea8f72b9cabed0ee336ff
Krai53n <greasha46@gmail.com>
2022-03-29 17:38:51 +0300

    add scene

--- 2_term/labs/4/snake.cpp
+++ 2_term/labs/4/snake.cpp
@@ -4,31 +4,50 @@
 #include "window.h"
 
 struct Snake {
-	Uint8 blockSize = 20;
+	Uint8 blockSize = 14;
 	SDL_Rect head;
 	SDL_Rect* body = NULL;
 	SDL_Color color = { 120, 255, 255 };
 };
 
-int main(int argc, char* argv[]) {
-    init();
+struct Scene {
+	Snake snake;
+};
+
+int processEvents(SDL_Event ev, Scene &scene) {
+		if (ev.type == SDL_QUIT) 
+			return 0;
+}
+
+int gameLoop() {
+	SDL_Event ev;
+	
+	int bodyLen = 0;
+	Scene scene;
+
+	while (true) {
+		SDL_PollEvent(&ev);
+		processEvents(ev, scene);
 
 	SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
 	SDL_RenderClear(ren);
 
 	Snake s;
-	int bodyLen = 10;
-	s.body = (SDL_Rect*)malloc(sizeof(SDL_Rect) * bodyLen);
-	for (int i = 0; i < bodyLen; i++)
-	{
+	s.body = (SDL_Rect*)realloc(s.body, sizeof(SDL_Rect) * ++bodyLen);
+	for (int i = 0; i < bodyLen; i++) {
 		s.body[i] = { i * s.blockSize + s.blockSize, s.blockSize, s.blockSize, s.blockSize };
-		SDL_SetRenderDrawColor(ren, s.color.r - i * 20, s.color.g - i * 10, s.color.b - i * 10, 255);
+		SDL_SetRenderDrawColor(ren, s.color.r - i * 1, s.color.g - i * 4.5, s.color.b - i * 4.5, 255);
 		SDL_RenderFillRect(ren, &s.body[i]);
 	}
 
-	SDL_RenderPresent(ren);
-	SDL_Delay(1000);
+		SDL_RenderPresent(ren);
+		SDL_Delay(60);
+	}
+}
 
+int main(int argc, char* argv[]) {
+    init();
+	gameLoop();
     deInit(0);
 
 	return 0;
