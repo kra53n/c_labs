65202c53643f1b4f3bc28fd50af75a5fbc990627
Krai53n <greasha46@gmail.com>
2022-04-01 22:45:17 +0300

    fix rain

--- 2_term/labs/4/rain.cpp
+++ 2_term/labs/4/rain.cpp
@@ -1,8 +1,8 @@
-#include <SDL2/SDL.h>
 #include <malloc.h>
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <SDL.h>
 #include "window.h"
 
 struct Raindrop
@@ -24,19 +24,19 @@ struct RainLayer
 struct Rain
 {
     RainLayer* rainLayers = NULL;
-    int amount;
+    int amount = 0;
 };
 
-void updateraindropLayer(RainLayer &rainLayer)
+void updateRaindropLayer(RainLayer& rainLayer)
 {
     for (int rndrop = 0; rndrop < rainLayer.amount; rndrop++)
     {
-        rainLayer.raindrops[rndrop].x += rainLayer.raindrops[rndrop].len * 0.2;
-        rainLayer.raindrops[rndrop].y += rainLayer.raindrops[rndrop].len * 0.2;
+        rainLayer.raindrops[rndrop].x += rainLayer.raindrops[rndrop].len * 1;
+        rainLayer.raindrops[rndrop].y += rainLayer.raindrops[rndrop].len * 1;
     }
 }
 
-void resetRainLayer(RainLayer &rainLayer, float direction)
+void resetRainLayer(RainLayer& rainLayer, float direction)
 {
     int x_range = winWdt;
     int y_range = winHgt;
@@ -44,22 +44,20 @@ void resetRainLayer(RainLayer &rainLayer, float direction)
     int rndrop = 0;
     for (int y = y_range; y > 0; y -= rainLayer.distance, rndrop++)
     {
-        rainLayer.raindrops = (Raindrop*)realloc(rainLayer.raindrops, sizeof(Raindrop) * (rndrop+1));
-        rainLayer.raindrops[rndrop].x = 0;
-        rainLayer.raindrops[rndrop].y = y + rand() % 50;
+        rainLayer.raindrops = (Raindrop*)realloc(rainLayer.raindrops, sizeof(Raindrop) * (rndrop + 1));
+        rainLayer.raindrops[rndrop].x = -(rand() % 100);
+        rainLayer.raindrops[rndrop].y = y - rand() % 100;
         rainLayer.raindrops[rndrop].len = rand() % 10 * 10;
         rainLayer.raindrops[rndrop].direction = direction;
-        //printf("mmmx: %f y: %f\n", RainLayer.raindrops[rndrop].x, RainLayer.raindrops[rndrop].y);
     }
 
     for (int x = 0; x < x_range; x += rainLayer.distance, rndrop++)
     {
-        rainLayer.raindrops = (Raindrop*)realloc(rainLayer.raindrops, sizeof(Raindrop) * (rndrop+1));
-        rainLayer.raindrops[rndrop].x = x + rand() % 50;
-        rainLayer.raindrops[rndrop].y = 0;
+        rainLayer.raindrops = (Raindrop*)realloc(rainLayer.raindrops, sizeof(Raindrop) * (rndrop + 1));
+        rainLayer.raindrops[rndrop].x = x - rand() % 100;
+        rainLayer.raindrops[rndrop].y = -(rand() % 100);
         rainLayer.raindrops[rndrop].len = rand() % 10 * 10;
         rainLayer.raindrops[rndrop].direction = direction;
-        //printf("x: %f y: %f\n", RainLayer.raindrops[rndrop].x, RainLayer.raindrops[rndrop].y);
     }
     rainLayer.amount = rndrop++;
 }
@@ -74,20 +72,20 @@ void drawRainLayer(RainLayer rainLayer)
             ren,
             rainLayer.raindrops[rndrop].x,
             rainLayer.raindrops[rndrop].y,
-            rainLayer.raindrops[rndrop].x + cos(angle) * rainLayer.raindrops[rndrop].len,
-            rainLayer.raindrops[rndrop].y + sin(angle) * rainLayer.raindrops[rndrop].len
+            rainLayer.raindrops[rndrop].x - cos(angle) * rainLayer.raindrops[rndrop].len,
+            rainLayer.raindrops[rndrop].y - sin(angle) * rainLayer.raindrops[rndrop].len
         );
     }
 }
 
-void addRainLayer(Rain& rain, int idx)
+void addRainLayer(Rain& rain)
 {
     RainLayer rainLayer; rainLayer.distance = 10; rainLayer.speed = 10;
-    resetRainLayer(rainLayer, 1.3);
+    resetRainLayer(rainLayer, 2);
 
+    rain.rainLayers = (RainLayer*)realloc(rain.rainLayers, sizeof(RainLayer) * (rain.amount+1));
+    rain.rainLayers[rain.amount] = rainLayer;
     rain.amount++;
-    rain.rainLayers = (RainLayer*)realloc(rain.rainLayers, sizeof(RainLayer) * rain.amount);
-    rain.rainLayers[idx] = rainLayer;
 }
 
 int main(int argc, char* artgv[])
@@ -98,25 +96,26 @@ int main(int argc, char* artgv[])
     RainLayer rainLayer; rainLayer.distance = 10; rainLayer.speed = 10;
 
     Rain rain;
-    addRainLayer(rain, 1);
+    addRainLayer(rain);
 
     for (int period = 1; period < 100; period++)
     {
         SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
         SDL_RenderClear(ren);
 
-        if (period % 20 == 0)
-            addRainLayer(rain, rain.amount+1);
+        if (period % 1 == 0)
+            addRainLayer(rain);
 
         for (int idx = 0; idx < rain.amount; idx++)
         {
             drawRainLayer(rain.rainLayers[idx]);
-            updateraindropLayer(rain.rainLayers[idx]);
+            updateRaindropLayer(rain.rainLayers[idx]);
         }
 
         SDL_RenderPresent(ren);
         SDL_Delay(60);
     }
+    free(rain.rainLayers);
 
     deInit(0);
     return 0;
