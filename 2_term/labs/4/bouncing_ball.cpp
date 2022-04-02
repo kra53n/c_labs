6dadb28470c46934d64f02b82252517e148b9792
Krai53n <greasha46@gmail.com>
2022-04-02 11:56:10 +0300

    simplified ball

--- 2_term/labs/4/bouncing_ball.cpp
+++ 2_term/labs/4/bouncing_ball.cpp
@@ -1,21 +1,20 @@
 #include <SDL2/SDL.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-SDL_Window*   win = NULL;
+SDL_Window* win = NULL;
 SDL_Renderer* ren = NULL;
 
 int winWdt = 800;
 int winHgt = 600;
 
-struct Circ{
+struct Circ {
     float x;
     float y;
     float r;
-    float xs=1; // x stretching
-    float ys=1; // y stretching
     float x_speed = 1;
-    float y_speed = 10;
+    float y_speed = 1;
     float acceleration = 1;
 };
 
@@ -37,7 +36,7 @@ void init()
         deInit(1);
     }
 
-    win = SDL_CreateWindow(u8"Снегопад", SDL_WINDOWPOS_CENTERED,
+    win = SDL_CreateWindow("BB | Bouncing Ball", SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED, winWdt, winHgt, SDL_WINDOW_SHOWN
     );
     if (win == NULL)
@@ -56,19 +55,19 @@ void init()
 
 void drawFillCric(Circ c)
 {
-    for (int w = 0; w < c.xs * c.r * 2; w++)
+    for (int w = 0; w < c.r * 2; w++)
     {
-        for (int h = 0; h < c.ys * c.r * 2; h++)
+        for (int h = 0; h < c.r * 2; h++)
         {
-            int dx = c.xs * c.r - w;
-            int dy = c.ys * c.r - h;
-            if (((dx * dx) / (c.xs * c.xs) + (dy * dy) / (c.ys * c.ys)) <= (c.r * c.r))
+            int dx = c.r - w;
+            int dy = c.r - h;
+            if (dx * dx + dy * dy <= c.r * c.r)
                 SDL_RenderDrawPoint(ren, (c.x + dx), (c.y + dy));
         }
     }
 }
 
-void screenCircCollision(Circ &c)
+void screenCircCollision(Circ& c)
 {
     float deformation_speed = 1 * fabs(c.y_speed);
 
@@ -83,55 +82,36 @@ void screenCircCollision(Circ &c)
         c.x_speed *= -1; c.x = winWdt - c.r;
     }
 
-     // vertical collision
-     if (c.y - c.r < 0)
-     {
-         c.y_speed *= -1;
-     }
-
-    if (c.y + c.r * c.ys > winHgt)
+    // vertical collision
+    if (c.y - c.r < 0)
     {
-        c.ys -= 1 / deformation_speed;
-        c.xs += 1 / deformation_speed;
-        c.y += c.r / deformation_speed;
-
-        if (c.ys <= 0.8)
-        {
-            c.y = winHgt - c.r * c.ys -1;
-            c.y_speed *= -1;
-            c.y_speed += c.acceleration;
-            c.acceleration += 0.1;
-        }
+        c.y_speed *= -1;
     }
 
-    if (c.y_speed < 0)
+    if (c.y + c.r > winHgt)
     {
-        if (c.ys < 1)
-        {
-            c.ys += 1 / deformation_speed;
-            c.xs -= 1 / deformation_speed;
-            c.y -= c.r / deformation_speed;
-        }
+
+        c.y = winHgt - c.r - 1;
+        c.y_speed *= -0.8;
+        if (fabs(c.y_speed) <= 1.777)
+            c.y_speed = -70;
     }
 }
 
-void updateCirc(Circ &c)
+void updateCirc(Circ& c)
 {
-    screenCircCollision(c);
     c.x += 0 * c.x_speed;
+    c.y_speed += c.acceleration;
+    c.y += c.y_speed;
 
-    if (c.ys >= 1)
-    {
-        c.y_speed += c.acceleration;
-        c.y += c.y_speed;
-    }
+    screenCircCollision(c);
 }
 
 void mainLoop()
 {
     SDL_Event ev;
 
-    Circ circ = { winWdt/2, 200, 50, 1, 1 };
+    Circ circ = { winWdt / 2, 200, 50, 1, 1 };
 
     bool isRunning = true;
     while (isRunning)
@@ -139,13 +119,13 @@ void mainLoop()
         SDL_PollEvent(&ev);
         if (ev.type == SDL_QUIT)
             isRunning = false;
+        updateCirc(circ);
 
         SDL_SetRenderDrawColor(ren, 40, 40, 40, 255);
         SDL_RenderClear(ren);
         SDL_SetRenderDrawColor(ren, 80, 73, 69, 255);
 
         drawFillCric(circ);
-        updateCirc(circ);
 
         SDL_Delay(10);
         SDL_RenderPresent(ren);
