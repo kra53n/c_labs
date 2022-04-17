0c2f1ffed750bb3b299657fbcd75fab38a2bf8be
Krai53n <greasha46@gmail.com>
2022-04-17 21:55:12 +0300

    add sum and product

--- 2_term/labs/13/main.cpp
+++ 2_term/labs/13/main.cpp
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include "stack.h"
 
+int stackProduct(Stack* s)
+{
+	int product = 1;
+	for (int i = 0; i < s->len; i++)
+		product *= (int)s->elems[i];
+	return (int)product;
+}
+
+int stackSum(Stack* s)
+{
+	int sum = 0;
+	for (int i = 0; i < s->len; i++)
+		sum += (int)s->elems[i];
+	return (int)sum;
+}
 
 int main()
 {
@@ -10,8 +25,11 @@ int main()
 	printf("Write a number: ");
 	for (char digit = getchar(); digit != '\n'; digit = getchar())
 		StackPush(&s, digit - 48);
+
 	printf("\nStack:\n");
 	StackPrint(&s);
-
+	printf("\nStack product: %d\nStack sum: %d\n", stackProduct(&s), stackSum(&s));
+	
+	StackDestroy(&s);
 	return 0;
 }
